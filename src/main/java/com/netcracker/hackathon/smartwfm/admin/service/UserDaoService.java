2d79a54bf3c818cbfe94f247b7bcb62859a86f58
smagar <sandip.magar@netcracker.com>
2023-08-27 09:54:48 +0530

    Sending email when new user is created

--- src/main/java/com/netcracker/hackathon/smartwfm/admin/service/UserDaoService.java
+++ src/main/java/com/netcracker/hackathon/smartwfm/admin/service/UserDaoService.java
@@ -3,12 +3,20 @@ package com.netcracker.hackathon.smartwfm.admin.service;
 import com.netcracker.hackathon.smartwfm.admin.model.User;
 import com.netcracker.hackathon.smartwfm.linemanager.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDaoService {
+
+    @Value("${mail.from}")
+    private String fromEmail;
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private MailCalendarService mailCalendarService;
 
     // TODO go for password Encryption from the UI and set the encrypted password
     // TODO in the table instead of plain string
@@ -21,6 +29,17 @@ public class UserDaoService {
     }
 
     public void save(User user) {
+        // TODO Logic to generate random password
         userRepository.save(user);
+        mailCalendarService.sendSimpleEmail(
+                new CalendarRequest.Builder()
+                        .withSubject("Your SMARTWFM Account has been created")
+                        .withBody("Hello " + user.getUserName() + ", Your SMARTWFM account has been created. please login using username: " +
+                                user.getEmailId() +" and password: " + user.getPassword())
+                        .withFromEmail(fromEmail)
+                        .withToEmail("sandip.magar@netcracker.com")
+                        .build()
+        );
+
     }
 }
