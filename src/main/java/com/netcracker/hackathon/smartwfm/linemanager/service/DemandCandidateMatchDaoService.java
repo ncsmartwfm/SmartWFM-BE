355013eb303fdb5ae9e64833a16244dbebf513f5
smagar <sandip.magar@netcracker.com>
2023-08-27 14:12:06 +0530

    Changes related to sending Email to Domain Owner Approval

--- src/main/java/com/netcracker/hackathon/smartwfm/linemanager/service/DemandCandidateMatchDaoService.java
+++ src/main/java/com/netcracker/hackathon/smartwfm/linemanager/service/DemandCandidateMatchDaoService.java
@@ -1,17 +1,36 @@
 package com.netcracker.hackathon.smartwfm.linemanager.service;
 
+import com.netcracker.hackathon.smartwfm.admin.service.MailCalendarService;
 import com.netcracker.hackathon.smartwfm.linemanager.dao.DemandCandidateMatch;
+import com.netcracker.hackathon.smartwfm.linemanager.dao.LifeCycleStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DemandCandidateMatchDaoService {
+
+    private static final String SMARTWFM_DO_APPROVAL_SENT = "Domain Owner Approval Needed For Demand";
+
+    @Value("${mail.from}")
+    private String fromEmail;
+
+    @Autowired
+    private MailCalendarService mailCalendarService;
+
     @Autowired
     private DemandCandidateMatchRepository demandCandidateMatchRepository;
 
     public void saveDemandCandidateMatchRecord(DemandCandidateMatch demandCandidateMatch) {
+        if (LifeCycleStatus.WAITING_FOR_DO_APPROVAL.equals(demandCandidateMatch.getLifecycleStatus()))
+        try {
+            String body = "Hello Sandip, Please provide your approval on SMARTWFM for Demand to schedule interview ";
+            mailCalendarService.sendMailNotification(SMARTWFM_DO_APPROVAL_SENT, body, fromEmail, "manvendrav@gmail.com");
+        } catch (Exception exception) {
+            throw exception;
+        }
         demandCandidateMatchRepository.save(demandCandidateMatch);
     }
 
